9d8cf5bd8c7d16a3924833c8f2866b58e7466588
moneyjoshua <89445479+moneyjoshua@users.noreply.github.com>
2022-12-18 01:36:45 +0300

    Initial commit

---
+++ tree/tree.cpp
@@ -0,0 +1,11 @@
+#include "tree.hpp"
+
+auto tree::insert(int val) -> tree_node * {
+    // Код для добавления элемента в дерево
+    return nullptr;
+}
+
+auto tree::remove(int val) -> bool {
+    // Код для удаления элемента из дерева
+    return false;
+}
